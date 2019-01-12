6efa76fcdda4f2a2c7f69ad4c18f76875e9c79bf
butachin <b1016094@fun.ac.jp>
2019-01-12 16:55:23 +0900

    change interface and add Card

--- src/components/WeatherCard/WeatherCard.tsx
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -1,43 +1,68 @@
-import * as React from 'react';
-import {Panel} from 'react-bootstrap';
+import * as React from "react";
+import { Card, Avatar } from "antd";
+
+const { Meta } = Card;
 
 export interface IWeatherCard {
-    dt: number,
-    main: {
-        temp: number,
-        temp_min: number,
-        temp_max: number,
-        pressure: number,
-        sea_level: number,
-        grnd_level: number,
-        humidity: number,
-        temp_kf: number
-    },
-    weather: [
-        {
-            id: number,
-            main: string,
-            description: string,
-            icon: string
-        }
-    ],
-    dt_txt: string
+  dt: number;
+  main: {
+    temp: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: [
+    {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }
+  ];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  sys?: {
+    pod: string;
+  };
+  dt_txt: string;
 }
 
 class WeatherCard extends React.Component<IWeatherCard, any> {
-    constructor(props: any) {
-        super(props);
-    }
-    public render() {
-        return (
-            <div>
-                <Panel className="Panel">
-                <Panel.Heading>日時: {this.props.dt_txt}</Panel.Heading>
-                <Panel.Body/>
-                </Panel>
-            </div>
-        )
-    }
+  constructor(props: any) {
+    super(props);
+  }
+
+  private kelvinToCelsius = (temp: number): number => Math.round(temp - 273.15);
+
+  public render() {
+    return (
+      <div className="Card">
+        <Card 
+        style={{ width: 800 }}
+        >
+          <Meta
+            avatar={<Avatar src={`http://openweathermap.org/img/w/${this.props.weather[0].icon}.png`}size="large"/>}
+            title={
+              <div>
+                <p>{this.props.weather[0].main}</p>
+                <h3>{this.kelvinToCelsius(this.props.main.temp) + "°C"}</h3>
+              </div>
+            }
+            description={this.props.dt_txt}
+          />
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default WeatherCard;
