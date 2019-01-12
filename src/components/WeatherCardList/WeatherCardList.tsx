d9c9e56fab0314ceb5d1bc5fdba84bc121937773
butachin <b1016094@fun.ac.jp>
2019-01-12 16:57:04 +0900

    little correction

--- src/components/WeatherCardList/WeatherCardList.tsx
+++ src/components/WeatherCardList/WeatherCardList.tsx
@@ -1,52 +1,57 @@
-import * as React from 'react';
+import * as React from "react";
 import * as request from "superagent";
-import WeatherCard, { IWeatherCard } from '../WeatherCard/WeatherCard';
-import * as dotenv from 'dotenv';
+import WeatherCard, { IWeatherCard } from "../WeatherCard/WeatherCard";
+import * as dotenv from "dotenv";
+import { Jumbotron　} from 'react-bootstrap';
 
 dotenv.config();
 
 interface IWeatherCardList {
-    weatherCards: IWeatherCard[];
+  weatherCards: IWeatherCard[];
 }
 
 class WeatherCardList extends React.Component<any, IWeatherCardList> {
-    constructor(props: any) {
-        super(props)
-        this.state = {
-            weatherCards: []
-        };
-    }
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      weatherCards: []
+    };
+  }
 
-    public componentWillMount() {
-        request
-          .get(
-            "http://api.openweathermap.org/data/2.5/forecast"
-          )
-          .query({
-              q: 'Hakodate',
-              appid: process.env.REACT_APP_API_KEY
-          })
-          .then(response => response.body.list)
-          .then(body => {
-              console.log(body)
-              this.setState({
-                weatherCards: body,
-              })
-              console.log(JSON.stringify(this.state.weatherCards))
-          }
-          )
-          .catch(error => {
-            console.log(error);
-            throw error;
-          });
-      }
-      public render() {
-        const resultNodes = this.state.weatherCards.map(weatherCard => {
-          return <WeatherCard key={weatherCard.dt} {...weatherCard} />;
+  public componentWillMount() {
+    request
+      .get("http://api.openweathermap.org/data/2.5/forecast")
+      .query({
+        q: "Hakodate",
+        appid: process.env.REACT_APP_API_KEY
+      })
+      .then(response => response.body.list)
+      .then(body => {
+        console.log(body);
+        this.setState({
+          weatherCards: body
         });
+        console.log(JSON.stringify(this.state.weatherCards));
+      })
+      .catch(error => {
+        console.log(error);
+        throw error;
+      });
+  }
+  public render() {
+    const resultNodes = this.state.weatherCards.map(weatherCard => {
+      return <WeatherCard key={weatherCard.dt} {...weatherCard} />;
+    });
 
-        return <ul>{resultNodes}</ul>;
-      }
-} 
+    return (
+    <div>
+        <Jumbotron>
+          <h1>函館お天気情報</h1>
+          <p>Hakodate Weather Information</p>
+        </Jumbotron>
+        <ul>{resultNodes}</ul>
+    </div>
+    )};
+}
 
 export default WeatherCardList;
