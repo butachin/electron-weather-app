cb4834b7ef911f712a0de45eedff46727f720ce2
butachin <b1016094@fun.ac.jp>
2019-01-31 13:38:13 +0900

    [add]

--- Server/Repositories/WeatherRepository.cs
+++ Server/Repositories/WeatherRepository.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Net;
+using Server.Models;
+using Server.Repositories.Endpoints;
+using Server.Repositories.Interfaces;
+
 namespace Server.Repositories
 {
-    public class WeatherRepository
+    public class WeatherRepository:IWeatherRepository
     {
-        public WeatherRepository()
+        public List<OpenWeather> FindOpenWeatherByCityName(string cityName)
         {
+            string ROOT_URL = OpenWeatherMap.ROOT_URL;
+            string APP_KEY = OpenWeatherMap.APP_KEY;
+            string url = ROOT_URL + APP_KEY;
+            var weatherList = new List<OpenWeather>();
+            using(WebClient webClient = new WebClient())
+            {
+                string json = webClient.DownloadString(url);
+                OpenWeather openWeather = OpenWeather.FromJson(json);
+                weatherList.Add(openWeather);
+            }
+            return weatherList;
         }
     }
 }
