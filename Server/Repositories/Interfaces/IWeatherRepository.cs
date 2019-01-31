3956ea06cfe210a10c8f6b82d63474394396498b
butachin <b1016094@fun.ac.jp>
2019-01-31 13:33:37 +0900

    [add] WeatherRepository

--- Server/Repositories/Interfaces/IWeatherRepository.cs
+++ Server/Repositories/Interfaces/IWeatherRepository.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Server.Models;
+
 namespace Server.Repositories.Interfaces
 {
     public interface IWeatherRepository
     {
+        List<OpenWeather> FindOpenWeatherByCityName(string cityName);
     }
 }
