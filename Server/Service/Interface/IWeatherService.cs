e9e2bb6dd44610fd64e37fda8973e2ddff29f03b
butachin <b1016094@fun.ac.jp>
2019-02-02 16:37:19 +0900

    [add] IWeatherService

--- Server/Service/Interface/IWeatherService.cs
+++ Server/Service/Interface/IWeatherService.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Proto.Weather;
+
 namespace Server.Service.Interface
 {
     public interface IWeatherService
     {
+        List<Weather> FindOpenWeatherByCityName(string cityName);
     }
 }
