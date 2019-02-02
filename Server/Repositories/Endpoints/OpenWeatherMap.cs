bee85f57d129e5e118705c4bff041b3de4c11fd6
butachin <b1016094@fun.ac.jp>
2019-02-02 14:27:49 +0900

    [delete] cityName

--- Server/Repositories/Endpoints/OpenWeatherMap.cs
+++ Server/Repositories/Endpoints/OpenWeatherMap.cs
@@ -2,7 +2,7 @@
 {
     public static class OpenWeatherMap
     {
-        public static string ROOT_URL = "http://api.openweathermap.org/data/2.5/forecast?q=Hakodate";
+        public static string ROOT_URL = "http://api.openweathermap.org/data/2.5/forecast"; // ?q=Hakodate
         public static string APP_KEY = "&appid=742c5ceb44cc03def377fa696628f8d2";
     }
 }
