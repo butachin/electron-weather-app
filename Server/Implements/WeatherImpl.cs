a0e06747d0269989598fe6f519a804efe89b78fe
butachin <b1016094@fun.ac.jp>
2019-02-02 17:02:05 +0900

    [add] WeatherImpl

--- Server/Implements/WeatherImpl.cs
+++ Server/Implements/WeatherImpl.cs
@@ -1,13 +1,25 @@
+using System;
+using System.Threading.Tasks;
+using Grpc.Core;
 using Proto.Weather;
+using Server.Service.Interface;
 
-namespace Repositories.Implements
+namespace Server.Implements
 {
     public class WeatherImpl: Weathers.WeathersBase
     {
         IWeatherService service;
         public WeatherImpl(IWeatherService _service): base()
         {
-
+            this.service = _service ?? throw new ArgumentNullException(nameof(IWeatherService));
+        }
+        public override Task<GetResponse> Get(GetRequest request, ServerCallContext context)
+        {
+            string cityName = request.CityName;
+            var weathers = this.service.FindOpenWeatherByCityName(cityName);
+            var response = new GetResponse();
+            response.Weather = weathers[0];
+            return Task.FromResult(response);
         }
     }
 }
