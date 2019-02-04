dd258e1cc35c9f3476880ad0bb13eda95efb1079
butachin <b1016094@fun.ac.jp>
2019-02-04 17:35:49 +0900

    [update] Program

---
+++ Backend/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Grpc.Core;
+using Microsoft.Extensions.Configuration;
+using Proto.Weather;
+using Backend.Implements;
+using Backend.Service;
+
+namespace Backend
+{
+    public class Program
+    {
+        public static IConfigurationRoot configuration;
+        public static void Main(string[] args)
+        {
+            const int Port = 5000;
+            var weatherImple = new WeatherImpl(new WeatherService());
+            Server server = new Server
+            {
+                Services =
+                {
+                    Weathers.BindService(weatherImple)
+                },
+                Ports = { new ServerPort("0.0.0.0", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.Read();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
