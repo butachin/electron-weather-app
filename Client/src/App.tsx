27c53e3d7c493b24ee1cefb8b9213afe5d3a7afa
butachin <b1016094@fun.ac.jp>
2019-02-09 18:24:41 +0900

    [update]

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,12 +1,26 @@
 import * as React from "react";
 import "./App.css";
+import SearchCityName from './containers/SearchCityName/SearchCityName';
+import IScreenState, { ScreenType } from './states/ScreenState';
 import WeatherCardList from "./components/WeatherCardList/WeatherCardList";
 
-class App extends React.Component {
+export interface IAppConnectedProps {
+  screenState: IScreenState
+}
+
+class App extends React.Component<IAppConnectedProps> {
   public render() {
+    const {screenState} = this.props;
     return (
       <div className="App">
-        <WeatherCardList />
+        {
+          screenState.screenType === ScreenType.SEARCH_SCREEN &&
+          <SearchCityName /> 
+        }
+        {
+          screenState.screenType === ScreenType.RESULT_SCREEN &&
+          <WeatherCardList />
+        }
       </div>
     );
   }
