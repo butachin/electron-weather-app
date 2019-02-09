0121ca724c20c16d82b79b2155d9af1516b38ee9
butachin <b1016094@fun.ac.jp>
2019-02-09 17:57:08 +0900

    [add] ScreenState

--- Client/src/states/index.ts
+++ Client/src/states/index.ts
@@ -1,5 +1,7 @@
 import { ICityNameState } from './CityNameState';
+import IScreenState from './ScreenState';
 
 export default interface IRootState {
     cityNameState: ICityNameState;
+    screenState: IScreenState;
 }
