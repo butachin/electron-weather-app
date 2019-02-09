99405b00abb7453bcbdd5deac1a5c4cbb82f7676
butachin <b1016094@fun.ac.jp>
2019-02-09 17:31:23 +0900

    [add] store

--- Client/src/index.tsx
+++ Client/src/index.tsx
@@ -3,9 +3,20 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
+import ReduxLogger from 'redux-logger';
+import { Provider } from 'react-redux';
 
+const store = createStore(
+  rootReducer,
+  applyMiddleware(ReduxLogger)
+)
 ReactDOM.render(
-  <App />,
+  <Provider store = {store}>
+  <App />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
+
 registerServiceWorker();
