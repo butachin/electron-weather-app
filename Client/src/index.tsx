b152b84fed114a2d726617af344314890dac5722
butachin <b1016094@fun.ac.jp>
2019-02-09 18:25:34 +0900

    [change] App

--- Client/src/index.tsx
+++ Client/src/index.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 import ReduxLogger from 'redux-logger';
 import { Provider } from 'react-redux';
+import App from './containers/App/App';
 
 const store = createStore(
   rootReducer,
@@ -14,7 +14,7 @@ const store = createStore(
 )
 ReactDOM.render(
   <Provider store = {store}>
-  <App />
+   <App />
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
