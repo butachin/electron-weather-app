b9e2ff363432d05dff7926d2211d56b03f5ecfb6
butachin <b1016094@fun.ac.jp>
2019-02-09 18:12:57 +0900

    [add] switchResultScreen

--- Client/src/containers/SearchCityName/SearchCityName.ts
+++ Client/src/containers/SearchCityName/SearchCityName.ts
@@ -4,11 +4,16 @@ import { ISearchCityNameProps } from 'src/components/SearchCityName/SearchCityNa
 import { search } from 'src/actions/CityNameStateAction/CityNameActionCreater';
 import { connect } from 'react-redux';
 import { SearchCityName } from 'src/components/SearchCityName/SearchCityName';
+import { switchResultScreen } from 'src/actions/ScreenStateAction/ScreenActionCreater';
+import ScreenAction from 'src/actions/ScreenStateAction/ScreenAction';
 
-const mapDispatchToProps = (dispatch: Dispatch<CityNameAction>): ISearchCityNameProps => {
+const mapDispatchToProps = (dispatch: Dispatch<CityNameAction | ScreenAction>): ISearchCityNameProps => {
     return {
         search: (cityName: string) => {
             dispatch(search(cityName));
+        },
+        switchResultScreen: () => {
+            dispatch(switchResultScreen())
         }
     };
 };
