052e276cabca605a9eccd6d2ac2cf73e75a7b5cb
butachin <b1016094@fun.ac.jp>
2019-03-05 10:59:11 +0900

    [delete] Backend

--- Backend/Repositories/Endpoints/OpenWeatherMap.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Backend.Repositories.Endpoints
-{
-    public static class OpenWeatherMap
-    {
-        public static string ROOT_URL = "http://api.openweathermap.org/data/2.5/forecast"; // ?q=Hakodate
-        public static string APP_KEY = "&appid=742c5ceb44cc03def377fa696628f8d2";
-    }
-}
