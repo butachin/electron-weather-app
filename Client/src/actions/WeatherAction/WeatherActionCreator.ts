e00574115e2ef7120c2f168ecdd64ffbc23b27cc
butachin <b1016094@fun.ac.jp>
2019-03-16 12:07:58 +0900

    [add] WeatherAction

---
+++ Client/src/actions/WeatherAction/WeatherActionCreator.ts
@@ -0,0 +1,15 @@
+import * as WeatherActions from './WeatherAction';
+import { WeatherActionType } from './WeatherActionType';
+
+export const weatherRequest = (cityName: string): WeatherActions.WeatherRequestAction => ({
+    type: WeatherActionType.WEATHER_REQUEST,
+    cityName,
+});
+
+export const weatherSucceded = (): WeatherActions.WeatherSuccededAction => ({
+    type: WeatherActionType.WEATHER_SUCCEDED,
+});
+
+export const weatherFailed = (): WeatherActions.WeatherFailedAction => ({
+    type: WeatherActionType.WEATHER_FAILED,
+});
