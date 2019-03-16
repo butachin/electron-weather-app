af4c268b3254ad52376551154b46a9b61138c9c1
butachin <b1016094@fun.ac.jp>
2019-03-16 17:44:49 +0900

    [update] weatherRequest

--- Client/src/sagas/Weather/WeatherSaga.ts
+++ Client/src/sagas/Weather/WeatherSaga.ts
@@ -4,16 +4,35 @@ import * as WeatherPb from 'src/gRPC/proto/weather_pb';
 import { getWeather } from 'src/gRPC/client/WeatherClient';
 import { weatherSucceded, weatherFailed } from 'src/actions/WeatherAction/WeatherActionCreator';
 import { WeatherActionType } from 'src/actions/WeatherAction/WeatherActionType';
+import { Weathers } from 'src/states/WeatherState';
 
 function* weatherRequest(action: WeatherRequestAction) {
-    const {response}: {response: WeatherPb.GetResponse} = yield call(getWeather, "Hakodate")
-    const weatherList = response.getWeatherList()
+    const {response}: {response: WeatherPb.GetResponse} = yield call(getWeather, "Hakodate");
+    const weatherList = response.getWeatherList();
+    const weathers: Weathers = yield mapWeatherPbToWeathers(weatherList);
 
     if(weatherList) {
-        yield put(weatherSucceded());
+        yield put(weatherSucceded(weathers));
     } else {
         yield put(weatherFailed());
     }
+}
+
+const mapWeatherPbToWeathers = (weatherPb: WeatherPb.Weather[]): Weathers => {
+    const weathers: Weathers = weatherPb.map(w => (
+        {
+            id: w.getId(),
+            type: w.getType(),
+            temp: w.getTemp(),
+            temp_max: w.getTempmax(),
+            temp_min: w.getTempmin(),
+            wind: w.getWind(),
+            description: w.getDescription(),
+            icon: w.getIcon(),
+            dt_txt: w.getDttext()
+        }
+    ));
+    return weathers
 }
 
 export const WeatherSaga = [
