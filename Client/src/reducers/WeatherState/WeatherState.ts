836387f7da12000ef23e842c525ef48a7252ecd1
butachin <b1016094@fun.ac.jp>
2019-03-16 17:57:04 +0900

    [update] weatherState

--- Client/src/reducers/WeatherState/WeatherState.ts
+++ Client/src/reducers/WeatherState/WeatherState.ts
@@ -2,27 +2,15 @@ import WeatherAction from 'src/actions/WeatherAction/WeatherAction';
 import { WeatherActionType } from 'src/actions/WeatherAction/WeatherActionType';
 import { Weathers } from 'src/states/WeatherState';
 
-const initialState: Weathers = [
-    {
-    id: 0,
-    type: "",
-    temp: 0,
-    temp_max: 0,
-    temp_min: 0,
-    wind: 0,
-    description: "",
-    icon: "",
-    dt_txt: ""
-}
-]
+const initialState: Weathers = []
 
 const weatherState = (state: Weathers = initialState, action: WeatherAction): Weathers => {
     switch(action.type) {
         case WeatherActionType.WEATHER_SUCCEDED:
-        return {
-            ...state,
-            
-        }
+        return [
+            ...state.slice(state.length),
+            ...action.weatherList
+        ]
         default:
         return initialState;
     }
