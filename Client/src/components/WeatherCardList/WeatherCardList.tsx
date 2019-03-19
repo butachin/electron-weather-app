f9ee4616fd946e30916d55d0a0e1cb98bdbf796b
butachin <b1016094@fun.ac.jp>
2019-03-19 17:13:28 +0900

    [delete] didMount

--- Client/src/components/WeatherCardList/WeatherCardList.tsx
+++ Client/src/components/WeatherCardList/WeatherCardList.tsx
@@ -7,33 +7,7 @@ import WeatherCardListConnectedProps from './WeatherCardListProps';
 class WeatherCardList extends React.Component<WeatherCardListConnectedProps, WeatherCardListState> {
   constructor(props: WeatherCardListConnectedProps) {
     super(props);
-    this.state = {
-      weatherCards: []
-    };
   }
-
-  // public async componentDidMount() {
-  //   const { cityNameState } = this.props;
-  //   const res = await getWeather(cityNameState.cityName)
-  //   const weatherList: WeatherPb.Weather[] = await res.result.getWeatherList();
-　// 
-  //   const buf = weatherList.map(weather =>{
-  //    const a: IWeather = {
-　//      id: weather.getId(),
-　//      type: weather.getType(),
-  //      temp: weather.getTemp(),
-  //      temp_max: weather.getTempmax(),
-  //      temp_min: weather.getTempmin(),
-  //      wind: weather.getWind(),
-  //      description: weather.getDescription(),
-　//      icon: weather.getIcon(),
-  //      dt_txt: weather.getDttext()
-  //    }
-  //    this.state.weatherCards.push(a)
-  //  });
-  //  console.log(buf);
-  //  this.setState ({ weatherCards: this.state.weatherCards});
-  // }
 
   public render() {
     const resultNodes = this.props.weatherState.map(weatherCard => {
